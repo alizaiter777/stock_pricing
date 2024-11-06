4e865f188a807444fc5ca1e138d2c2ccce7cc437
alizaiter777 <alizaiter777@gmail.com>
2024-11-06 22:24:42 +0200

    generate maxprofit function

--- exc.py
+++ exc.py
@@ -2,3 +2,21 @@
 
 prices = list(map(int, input("Enter stock price in several days: ").split()))
 
+#search for the max price and min price 
+#then substract them and get profit 
+def max_profit(prices):
+  
+    max = prices[0]
+    min = prices[0]
+
+    for price in prices:
+        if price > max:
+            max = price
+
+        if price < min:
+            min = price
+
+    profit = max - min
+    return profit
+  
+print("Max profit is ",max_profit(prices))
